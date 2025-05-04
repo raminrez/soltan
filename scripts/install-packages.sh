e51964e22c570fea6e8df2ffd7fbfdd098a9e13f
Ramin Rezaei <ramin12207@gmail.com>
2025-05-05 02:18:44 +0330

    fix: install using wget

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -3,16 +3,19 @@
 set -euo pipefail
 export LC_ALL=C
 
+# Update package list
 echo "Updating package list..."
 sudo apt-get update
 
+# Install required packages
 echo "Installing WireGuard and iptables-persistent..."
 sudo apt-get install -y wireguard iptables-persistent
 
 # Install yq if not already installed
 if ! command -v yq &> /dev/null; then
     echo "yq could not be found, installing yq..."
-    sudo snap install yq
+    wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq
+    sudo chmod +x /usr/local/bin/yq
 fi
 
 # Install sed if not already installed
